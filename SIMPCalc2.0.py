7c9209ebf15d053c97b2820b29c1a1f0b6048206
Kennhope <62108078+Kennhope@users.noreply.github.com>
2020-10-23 18:56:10 -0400

    Create SIMPCalc2.0.py
    Here is SIMPCalc bEtA 2.0
    
    Bug fixed:
    - There was a bug, when only square root could be calculated, other functions were disabled.
    
    New:
    - Now, to calculate square root, you must enter the number you want to calculate first, then any number. It is what it is until I figure out how can it be done another way

---
+++ SIMPCalc2.0.py
@@ -0,0 +1,74 @@
+import math
+
+
+def add(x,y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x,y):
+    return x * y
+
+def divide(x,y):
+    return x / y
+
+def exp(x,y):
+    return x ** y
+
+def sqrt(x):
+    math.sqrt(x)
+
+
+
+print("Welcome to the SIMP Calc bEtA 1.0")
+print ("What do you want to do? :)")
+print ("+ Add")
+print ("- Subtract")
+print ("* Multiply")
+print ("/ Divide")
+print ("^ Exponentiation")
+print ("sqrt. Square Root")
+print ("WARNING. If you want to do square root, type in the number you want to calculate, then any number :)")
+
+while True:
+    choice = input("Enter your choice(sign): ")
+
+
+
+
+
+
+    if choice in ('+', '-', '*', '/', '^', 'sqrt'):
+        num1 = float(input("Enter your first number in here: "))
+        num2 = float(input("And another number in here: "))
+
+
+
+
+        if choice == '+':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '-':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '*':
+            print(num1, "x", num2, "=", multiply(num1, num2))
+
+        elif choice == '/':
+            print(num1, "/", num2, "=", divide(num1, num2))
+            break
+
+        elif choice == '^':
+            print(num1, "^", num2, "=", exp(num1, num2))
+
+        elif choice == 'sqrt':
+            print("√", num1, "=", math.sqrt(num1))
+
+
+
+
+
+
+    else:
+            print("Sorry, there is no function like that :(")
